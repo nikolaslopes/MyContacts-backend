e2923062a9b69f72ac74c4d81f811ade1a290cbf
Nix <nikolaslopes.dev@gmail.com>
2021-07-16 03:20:35 -0400

    added routers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.send('Hello World! ps: express is freaking easy');
-});
-
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
