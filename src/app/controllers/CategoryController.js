369b39180b94ac31e233bd5c885512d02797cb58
Nix <nikolaslopes.dev@gmail.com>
2021-07-20 04:22:49 +0000

    added update and delete to categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -3,22 +3,60 @@ const CategoriesRepository = require('../repositories/CategoriesRepository');
 class CategoryController {
   async index(request, response) {
     // To list all categories
-    const categories = await CategoriesRepository.findAll();
+    const { orderBy } = request.query;
+
+    const categories = await CategoriesRepository.findAll(orderBy);
 
     response.json(categories);
   }
 
   async store(request, response) {
-    // To create category
+    // To create one category
     const { name } = request.body;
 
     if (!name) {
       return response.status(400).json({ error: 'Name is required' });
     }
 
+    const categoryExists = await CategoriesRepository.findAll(name);
+
+    if (categoryExists) {
+      return response.status(400).json({ error: 'This category is already in use' });
+    }
+
     const category = await CategoriesRepository.create({ name });
 
     response.json(category);
+  }
+
+  async update(request, response) {
+    // To update one category
+    const { id } = request.params;
+
+    const { name } = request.body;
+
+    const categoryExists = await CategoriesRepository.findAll(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    // To delete one category
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
