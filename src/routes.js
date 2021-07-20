60c7171b8388dd06ba037d380dddb653a7f5d18b
Nix <nikolaslopes.dev@gmail.com>
2021-07-20 05:15:51 +0000

    organize routes

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,18 @@ const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
+// Contacts Routes
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
 
+// Categories Routes
 router.get('/categories', CategoryController.index);
-router.post('/categories', CategoryController.store);
+router.get('categories/:id', CategoryController.show);
 router.delete('/categories/:id', CategoryController.delete);
+router.post('/categories', CategoryController.store);
 router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
